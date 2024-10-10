78313ac30c67e522051930a2a82b42a0b97a689f
David Hamoui <90013257+david-hamoui@users.noreply.github.com>
2024-10-10 19:04:36 -0300

    timeline addition

---
+++ timeline.js
@@ -0,0 +1,13 @@
+document.addEventListener("scroll", function() {
+    const fadeInSections = document.querySelectorAll(".fade-in");
+
+    fadeInSections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        const sectionTop = rect.top;
+        const windowHeight = window.innerHeight;
+
+        if (sectionTop <= windowHeight - 100) {
+            section.classList.add("show");
+        }
+    });
+});
