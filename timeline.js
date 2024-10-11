f7cb8526a89c5f20f7caadfa023ff6173d51bb55
David Hamoui <90013257+david-hamoui@users.noreply.github.com>
2024-10-10 21:36:28 -0300

    js later fade in

--- timeline.js
+++ timeline.js
@@ -6,7 +6,7 @@ document.addEventListener("scroll", function() {
         const sectionTop = rect.top;
         const windowHeight = window.innerHeight;
 
-        if (sectionTop <= windowHeight - 150) {
+        if (sectionTop <= windowHeight - 180) {
             section.classList.add("show");
         }
     });
