e45d12f59cfb726bfe54278e61d7ec7f8a988ab9
David Hamoui <90013257+david-hamoui@users.noreply.github.com>
2024-10-28 19:16:02 -0300

    testing timeline

--- timeline.js
+++ timeline.js
@@ -6,7 +6,7 @@ document.addEventListener("scroll", function() {
         const sectionTop = rect.top;
         const windowHeight = window.innerHeight;
 
-        if (sectionTop <= windowHeight - 80) {
+        if (sectionTop <= windowHeight - 300) {
             section.classList.add("show");
         }
     });
